67b53857b47e24a1c5efa32b03254c9cc9dc84fe
Mohsen Iranmanesh <mhsn.iranmanesh@gmail.com>
2017-10-23 21:16:19 +0330

    sign in page validity

--- js/signin.js
+++ js/signin.js
@@ -12,13 +12,51 @@ var userName = $('#loginUserNameInput');
 var pass = $('#loginPassInput');
 
 function LogIn(){
+  if(userName.val() ===""){
+  $('.error-msg').remove();
+    var errorCross = document.createElement('i');
+    errorCross.setAttribute('class', 'fa fa-times-circle');
+    errorCross.setAttribute('aria-hidden', 'true');
+    var errorBox = document.createElement('span');
+  //  errorBox.id = 'errorBox';
+    errorBox.setAttribute('class' , 'error-msg');
+    errorBox.appendChild(errorCross);
+    var errorMessage = document.createElement('span');
+    errorMessage.innerHTML = 'لطفا نام کاربری را وارد کنید'
+    errorBox.appendChild(errorMessage);
+    $(errorMessage).prepend(errorCross);
+    $('#signUpForm').append(errorBox);
+  }
+else if(pass.val()===""){
+  $('.error-msg').remove();
+  var errorCross = document.createElement('i');
+  errorCross.setAttribute('class', 'fa fa-times-circle');
+  errorCross.setAttribute('aria-hidden', 'true');
+  var errorBox = document.createElement('span');
+//  errorBox.id = 'errorBox';
+  errorBox.setAttribute('class' , 'error-msg');
+  errorBox.appendChild(errorCross);
+  var errorMessage = document.createElement('span');
+  errorMessage.innerHTML = 'لطفا پسورد خود را وارد کنید'
+  errorBox.appendChild(errorMessage);
+  $(errorMessage).prepend(errorCross);
+  $('#signUpForm').append(errorBox);
+}
+else{
+  console.log('HHH');
+  $('.error-msg').remove();
+  LogInSendDataToServer();
+}
+}
+
+function LogInSendDataToServer(){
   $('.error-msg').remove();
 
   var data = {
     username : userName.val(),
     password : pass.val(),
   }
-  console.log("LOGINNNN : ", data);
+//  console.log("LOGINNNN : ", data);
   $.ajax({
     type : "POST",
     url: 'api/v1/auth/token/obtain/',
@@ -28,8 +66,10 @@ function LogIn(){
       window.location.href = "dashboard.html";
     },
     error : function (err) {
-      $('.error-msg').remove();
-      if(err.responseJSON.username &&  err.responseJSON.username.contains("This field is required.")){
+  //    $('.error-msg').remove();
+      console.log(err);
+      if(err.responseJSON.username &&  err.responseJSON.username.contains("This field may not be blank.")){
+//        console.log('add error');
         $('.error-msg').remove();
         var errorCross = document.createElement('i');
         errorCross.setAttribute('class', 'fa fa-times-circle');
@@ -44,7 +84,7 @@ function LogIn(){
         $(errorMessage).prepend(errorCross);
         $('#signUpForm').append(errorBox);
       }
-      if(err.responseJSON.password &&  err.responseJSON.password.contains("This field is required.")){
+      else if(err.responseJSON.password &&  err.responseJSON.password.contains("This field may not be blank.")){
         $('.error-msg').remove();
         var errorCross = document.createElement('i');
         errorCross.setAttribute('class', 'fa fa-times-circle');
@@ -59,7 +99,7 @@ function LogIn(){
         $(errorMessage).prepend(errorCross);
         $('#signUpForm').append(errorBox);
       }
-      if(err.responseJSON.non_field_errors &&  err.responseJSON.non_field_errors.contains("Unable to log in with provided credentials.")){
+      else if(err.responseJSON.non_field_errors &&  err.responseJSON.non_field_errors.contains("Unable to log in with provided credentials.")){
         $('.error-msg').remove();
         var errorCross = document.createElement('i');
         errorCross.setAttribute('class', 'fa fa-times-circle');
@@ -87,7 +127,7 @@ function LogIn(){
         errorMessage.innerHTML = 'خطا در اتصال به سرور ، لطفا مجدد تلاش کنید.'
         errorBox.appendChild(errorMessage);
         $(errorMessage).prepend(errorCross);
-        $('#signup-form').append(errorBox)
+        $('#signUpForm').append(errorBox)
 
       }
     }
